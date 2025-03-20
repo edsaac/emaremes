c77cf0fdfa2238b495a2a77b32a89c823cc1bae2
Ed Saa <esaavedrac@u.northwestern.edu>
2025-03-20 15:13:28 -0500

    Add to init

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from emaremes import download_data
 from emaremes import utils
+from emaremes import download_data
+from emaremes import timeseries
 
-__all__ = ["download_data", "utils"]
+__all__ = ["download_data", "utils", "timeseries"]
